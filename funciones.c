70f0e6a79d0439aeabcbaeb90050c7206aa074a7
KevMed24 <kevin.medina.pantoja@udla.edu.ec>
2024-07-02 08:46:40 -0500

    Taller_Final

--- funciones.c
+++ funciones.c
@@ -52,7 +52,6 @@ void realizarReserva(int numHabitacion, char habitaciones[9][3][40], char client
     printf("Ingresa tu cedula por favor: ");
     scanf("%s", cedula);
 
-  
     for (int i = 0; i < 10; i++) {
         if (reservas[i][0] == -1) {
             reservas[i][0] = numHabitacion;
@@ -81,10 +80,10 @@ void buscarReservaPorCedula(int *numReserva, char clientes[5][2][40], int reserv
     printf("Ingresa tu cedula: ");
     scanf("%s", cedula);
 
-    for (int i = 0; i < 5; i++) {
-        if (strcmp(clientes[i][1], cedula) == 0) {
-            *numReserva = reservas[i][1];
-            printf("Reserva encontrada para %s con cedula %s.\n", clientes[i][0], cedula);
+    for (int i = 0; i < 10; i++) {
+        if (reservas[i][0] != -1 && strcmp(clientes[reservas[i][1]][1], cedula) == 0) {
+            *numReserva = i + 1; 
+            printf("Reserva encontrada para %s con cedula %s.\n", clientes[reservas[i][1]][0], cedula);
             return;
         }
     }
@@ -94,12 +93,12 @@ void buscarReservaPorCedula(int *numReserva, char clientes[5][2][40], int reserv
 }
 
 void imprimirReserva(int numReserva, int reservas[10][4], char habitaciones[9][3][40], double precios[9]) {
-    if (numReserva == -1 || reservas[numReserva][0] == -1) {
+    if (numReserva == -1 || reservas[numReserva - 1][0] == -1) {
         printf("Reserva no encontrada.\n");
         return;
     }
 
-    int numHabitacion = reservas[numReserva][0];
+    int numHabitacion = reservas[numReserva - 1][0];
     printf("Detalle de la Reserva:\n");
     printf("Numero de Reserva: %d\n", numReserva);
     printf("Habitacion: %s\n", habitaciones[numHabitacion - 1][0]);
@@ -107,7 +106,7 @@ void imprimirReserva(int numReserva, int reservas[10][4], char habitaciones[9][3
     printf("Hotel: %s\n", habitaciones[numHabitacion - 1][2]);
     printf("Precio: %.2f\n", precios[numHabitacion - 1]);
 
-    if (reservas[numReserva][3] == 1) {
+    if (reservas[numReserva - 1][3] == 1) {
         printf("Estado de reserva: Activa\n");
     } else {
         printf("Estado de reserva: Pagada\n");
@@ -115,12 +114,12 @@ void imprimirReserva(int numReserva, int reservas[10][4], char habitaciones[9][3
 }
 
 void pagarReserva(int numReserva, int reservas[10][4], char habitaciones[9][3][40], double precios[9]) {
-    if (numReserva == -1 || reservas[numReserva][0] == -1) {
+    if (numReserva == -1 || reservas[numReserva - 1][0] == -1) {
         printf("Reserva no encontrada.\n");
         return;
     }
 
-    int numHabitacion = reservas[numReserva][0];
+    int numHabitacion = reservas[numReserva - 1][0];
     printf("Detalles de la reserva:\n");
     printf("Numero de Reserva: %d\n", numReserva);
     printf("Habitacion: %s\n", habitaciones[numHabitacion - 1][0]);
@@ -128,7 +127,7 @@ void pagarReserva(int numReserva, int reservas[10][4], char habitaciones[9][3][4
     printf("Hotel: %s\n", habitaciones[numHabitacion - 1][2]);
     printf("Precio: %.2f\n", precios[numHabitacion - 1]);
 
-    if (reservas[numReserva][3] == 0) {
+    if (reservas[numReserva - 1][3] == 0) {
         printf("La reserva ya ha sido pagada anteriormente.\n");
         return;
     }
@@ -138,9 +137,53 @@ void pagarReserva(int numReserva, int reservas[10][4], char habitaciones[9][3][4
     scanf("%d", &confirmacion);
 
     if (confirmacion == 1) {
-        reservas[numReserva][3] = 0;
+        reservas[numReserva - 1][3] = 0;
         printf("Reserva pagada con exito.\n");
     } else {
         printf("Pago cancelado.\n");
+    }
+}
+
+void guardarReservaEnArchivo(FILE *archivo, int numHabitacion, char habitaciones[9][3][40], char clientes[5][2][40], int reservas[10][4], double precios[9]) {
+    fseek(archivo, 0, SEEK_END);
+
+    for (int i = 0; i < 10; i++) {
+        if (reservas[i][0] != -1) {
+            fprintf(archivo, "Reserva realizada:\nCliente: %s\nCedula: %s\nHabitacion: %s (%s) en %s\nPrecio: %.2f\n\n",
+                    clientes[reservas[i][1]][0], clientes[reservas[i][1]][1],
+                    habitaciones[reservas[i][0] - 1][0], habitaciones[reservas[i][0] - 1][1],
+                    habitaciones[reservas[i][0] - 1][2], precios[reservas[i][0] - 1]);
+        }
+    }
+}
+
+void cargarReservasDesdeArchivo(FILE *archivo, char clientes[5][2][40], int reservas[10][4]) {
+    char buffer[200];
+    int i = 0;
+
+    fseek(archivo, 0, SEEK_SET);
+
+    while (fgets(buffer, sizeof(buffer), archivo) != NULL) {
+        if (strncmp(buffer, "Reserva realizada:", 18) == 0) {
+            fgets(buffer, sizeof(buffer), archivo);
+            sscanf(buffer, "Cliente: %s", clientes[i][0]);
+
+            fgets(buffer, sizeof(buffer), archivo);
+            sscanf(buffer, "Cedula: %s", clientes[i][1]);
+
+            fgets(buffer, sizeof(buffer), archivo);
+            int numHabitacion;
+            sscanf(buffer, "Habitacion: %d", &numHabitacion);
+
+            fgets(buffer, sizeof(buffer), archivo); 
+            fgets(buffer, sizeof(buffer), archivo);
+
+            reservas[i][0] = numHabitacion; 
+            reservas[i][1] = i; 
+            reservas[i][2] = 0; 
+            reservas[i][3] = 1; 
+
+            i++;
+        }
     }
 }
--- main.c
+++ main.c
@@ -1,94 +1,88 @@
-
 #include <stdio.h>
 #include "funciones.h"
 
-int main (){
+int main() {
+    char clientes[5][2][40] = { {"Juan", "1789654123"},
+                                {"", ""},
+                                {"", ""},
+                                {"", ""},
+                                {"", ""} };
 
-    char clientes[5][2][40]={{"Juan","1789654123"},
-                             {"",""},
-                             {"",""},
-                             {"",""},
-                             {"",""}};
+    char habitaciones[9][3][40] = { {"1", "simple", "Dan Carlton"},
+                                    {"2", "doble", "Dan Carlton"},
+                                    {"3", "triple", "Dan Carlton"},
+                                    {"4", "simple", "Swissotel"},
+                                    {"5", "doble", "Swissotel"},
+                                    {"6", "triple", "Swissotel"},
+                                    {"7", "simple", "Sheraton"},
+                                    {"8", "doble", "Sheraton"},
+                                    {"9", "triple", "Sheraton"} };
 
-    char habitaciones[9][3][40]={{"1","simple","Dan Carlton"},
-                                  {"2","doble","Dan Carlton"},
-                                  {"3","triple","Dan Carlton"},
-                                  {"4","simple","Swissotel"},
-                                  {"5","doble","Swissotel"},
-                                  {"6","triple","Swissotel"},
-                                  {"7","simple","Sheraton"},
-                                  {"8","doble","Sheraton"},
-                                  {"9","triple","Sheraton"}};
+    double precios[9] = {90, 120, 160, 65, 100, 140, 85, 110, 150};
 
-    double precios[9]={90,120,160,65,100,140,85,110,150};
+    int reservas[10][4] = { {-1, -1, -1, 0},
+                            {-1, -1, -1, 0},
+                            {-1, -1, -1, 0},
+                            {-1, -1, -1, 0},
+                            {-1, -1, -1, 0},
+                            {-1, -1, -1, 0},
+                            {-1, -1, -1, 0},
+                            {-1, -1, -1, 0},
+                            {-1, -1, -1, 0},
+                            {-1, -1, -1, 0} };
 
-    int reservas[10][4]={{-1,-1,-1,0},
-                         {-1,-1,-1,0},
-                         {-1,-1,-1,0},
-                         {-1,-1,-1,0},
-                         {-1,-1,-1,0},
-                         {-1,-1,-1,0},
-                         {-1,-1,-1,0},
-                         {-1,-1,-1,0},
-                         {-1,-1,-1,0},
-                         {-1,-1,-1,0}};
+    int opcion, numHabitacion, numReserva;
 
-    int opcion,numHabitacion,numReserva;
-
-    FILE *archivo = fopen("C:/Users/marce/OneDrive/Escritorio/reservas.txt", "a+");
-
+    FILE *archivo = fopen("reservas.txt", "a+");
     if (archivo == NULL) {
         printf("Error al abrir el archivo!\n");
+        return -1;
     }
 
-    do
-    {   
+    cargarReservasDesdeArchivo(archivo, clientes, reservas);
+    fclose(archivo);
+
+    do {
         printf("Escoja una opcion:\n 1.Buscar Habitacion\n 2.Realizar Reserva\n 3.Ver reservas\n 4.Pagar Reserva\n>>");
-        scanf("%d",&opcion);
-        switch (opcion)
-        {
-        case 1:
-            printf("1.Por tamano\n2.Por Hotel\n>>");
-            scanf("%d",&opcion);
-            switch (opcion)
-            {
+        scanf("%d", &opcion);
+        switch (opcion) {
             case 1:
-                buscarPorTamano(&numHabitacion,habitaciones,precios);
+                printf("1.Por tamano\n2.Por Hotel\n>>");
+                scanf("%d", &opcion);
+                switch (opcion) {
+                    case 1:
+                        buscarPorTamano(&numHabitacion, habitaciones, precios);
+                        break;
+                    case 2:
+                        buscarHotel(&numHabitacion, habitaciones, precios);
+                        break;
+                    default:
+                        break;
+                }
                 break;
             case 2:
-                buscarHotel(&numHabitacion,habitaciones,precios);      
+                realizarReserva(numHabitacion, habitaciones, clientes, reservas, precios);
+                archivo = fopen("reservas.txt", "a+");
+                guardarReservaEnArchivo(archivo, numHabitacion, habitaciones, clientes, reservas, precios);
+                fclose(archivo);
+                break;
+            case 3:
+                buscarReservaPorCedula(&numReserva, clientes, reservas);
+                imprimirReserva(numReserva, reservas, habitaciones, precios);
+                break;
+            case 4:
+                buscarReservaPorCedula(&numReserva, clientes, reservas);
+                pagarReserva(numReserva, reservas, habitaciones, precios);
+                archivo = fopen("reservas.txt", "a+");
+                guardarReservaEnArchivo(archivo, numReserva, habitaciones, clientes, reservas, precios);
+                fclose(archivo);
+                break;
             default:
                 break;
-            }
-            break;
-        case 2:
-            realizarReserva(numHabitacion,habitaciones,clientes,reservas,precios);
-            fprintf(archivo, "Reserva realizada:\nCliente: %s\nCédula: %s\nHabitación: %s (%s) en %s\nPrecio: %.2f\n\n",
-                    clientes[numHabitacion - 1][0], clientes[numHabitacion - 1][1], 
-                    habitaciones[numHabitacion - 1][0], habitaciones[numHabitacion - 1][1], 
-                    habitaciones[numHabitacion - 1][2], precios[numHabitacion - 1]);
-            break;
-        case 3:
-            buscarReservaPorCedula(&numReserva,clientes,reservas);
-            imprimirReserva(numReserva,reservas,habitaciones,precios);
-            fprintf(archivo, "Detalles de la reserva:\nNúmero de Reserva: %d\nHabitación: %s\nTipo de Habitación: %s\nHotel: %s\nPrecio: %.2f\n\n",
-                    numReserva, habitaciones[numReserva - 1][0], habitaciones[numReserva - 1][1], 
-                    habitaciones[numReserva - 1][2], precios[numReserva - 1]);
-            break;
-        case 4:
-            buscarReservaPorCedula(&numReserva,clientes,reservas);
-            pagarReserva(numReserva,reservas,habitaciones,precios);
-            fprintf(archivo, "Reserva pagada:\nNúmero de Reserva: %d\nHabitación: %s\nTipo de Habitación: %s\nHotel: %s\nPrecio: %.2f\n\n",
-                    numReserva, habitaciones[numReserva - 1][0], habitaciones[numReserva - 1][1], 
-                    habitaciones[numReserva - 1][2], precios[numReserva - 1]);
-            break;
-        default:
-            break;
         }
         printf("Desea elegir otra opcion\n1. Si\n2. No\n>>");
-        scanf("%d",&opcion);
-    } while (opcion==1);
-    
-    fclose(archivo); 
+        scanf("%d", &opcion);
+    } while (opcion == 1);
+
     return 0;
 }
